2153c2b0b9aca079382b8079fdc8c675bb92b887
Choi-y <atmolysis@naver.com>
2021-08-03 14:43:05 +0900

    [수정] 2839, 2953, 5565, 9037, 10156, 10757

--- workspace/10757.py
+++ workspace/10757.py
@@ -1,4 +1,9 @@
-# https://www.acmicpc.net/problem/10757 풀이
+# 큰 수 A+B (https://www.acmicpc.net/problem/10757) 풀이 (7/6)
+
+# 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+# 입력: 첫째 줄에 A와 B가 주어진다. (0 < A,B < 10^10000)
+# 파이썬이라서 쉽게 풀린 문제
 
 A, B = map(int, input().split())
 
