126ed175fb7ec3b538e24287f17d1e3dd85eb5dd
Choi-y <atmolysis@naver.com>
2021-08-03 14:32:08 +0900

    [수정] 2480, 2581, 2609

--- workspace/2581.py
+++ workspace/2581.py
@@ -1,10 +1,13 @@
-# https://www.acmicpc.net/problem/2581 풀이
+# 소수 (https://www.acmicpc.net/problem/2581) 풀이
+
+# 자연수 M과 N이 주어질 때 M이상 N이하의 자연수 중 소수인 것을 모두 골라 이들 소수의 합과 최솟값을 찾는 프로그램을 작성하시오.
+# 예를 들어 M=60, N=100인 경우 60이상 100이하의 자연수 중 소수는 61, 67, 71, 73, 79, 83, 89, 97 총 8개가 있으므로, 이들 소수의 합은 620이고, 최솟값은 61이 된다.
 
 M = int(input())
 N = int(input())
 
 sol = 0
-DEC = list()
+prime_number = list()
 for i in range(M, N+1):
     count = 0
     if i > 1:
@@ -13,10 +16,9 @@ for i in range(M, N+1):
                 count += 1
                 break
         if count == 0:
-            DEC.append(i)
+            prime_number.append(i)
 
-if len(DEC) == 0:
+if len(prime_number) == 0:
     print("-1")
 else:
-    print(sum(DEC))
-    print(min(DEC))
+    print("%d\n%d" % (sum(prime_number), min(prime_number)))
--- workspace/2609.py
+++ workspace/2609.py
@@ -1,17 +1,18 @@
-# https://www.acmicpc.net/problem/2609 풀이
+# 최대공약수와 최소공배수 (https://www.acmicpc.net/problem/2609) 풀이
+
+# 두 개의 자연수를 입력받아 최대 공약수와 최소 공배수를 출력하는 프로그램을 작성하시오.
 
 A, B = map(int, input().split())
 
 for i in range(1, min(A, B)+1):
 	if (A%i == 0) and (B%i == 0):
-		MAX = i
+		least_common_multiple = i
 
 j = max(A, B)
 while(1):
 	if (j%A == 0) and (j%B == 0):
-		MIN = j
+		greatest_common_factor = j
 		break
 	j += 1
 
-print(MAX)
-print(MIN)
+print("%d\n%d" % (least_common_multiple, greatest_common_factor))
